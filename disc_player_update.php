62530ccc658bc85c196024793c15c5caf7f1dd89
sud-Z <61097308+sud-Z@users.noreply.github.com>
2020-03-11 15:15:07 -0400

    Create disc_player_update.php

---
+++ disc_player_update.php
@@ -0,0 +1,125 @@
+<?php 
+
+session_start();
+if(!isset($_SESSION['player_id'])){ // if "user" not set,
+	session_destroy();
+	header('Location: login.php');     // go to login page
+	exit;
+}
+
+$id = $_SESSION['player_id']; // for MyAssignments
+$sessionid = $_SESSION['player_id'];
+	
+require 'database.php';
+
+if ( !empty($_POST)) { // if not first time through
+
+	// initialize user input validation variables
+	$fnameError = null;
+	$lnameError = null;
+	$mobileError = null;
+	
+    $fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$mobile = $_POST['mobile'];
+	
+	// validate user input
+	$valid = true;
+	if (empty($fname)) {
+		$fnameError = 'Please enter First Name';
+		$valid = false;
+	}
+	if (empty($lname)) {
+		$lnameError = 'Please enter Last Name';
+		$valid = false;
+	}
+	
+	if (empty($mobile)) {
+		$mobileError = 'Please enter Mobile Number (or "none")';
+		$valid = false;
+	}
+	if(!preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $mobile)) {
+		$mobileError = 'Please write Mobile Number in form 000-000-0000';
+		$valid = false;
+	}
+	
+		// update data
+				if ($valid) {
+			$pdo = Database::connect();
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sql = "UPDATE players set fname = ?, lname = ?, mobile =? WHERE id = ?";
+			$q = $pdo->prepare($sql);
+			$q->execute(array($fname,$lname,$mobile,$id));
+			Database::disconnect();
+			header("Location: disc_player_list.php");
+		}
+	} else {
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT * FROM players where id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		$data = $q->fetch(PDO::FETCH_ASSOC);
+		$fname = $data['fname'];
+		$lname = $data['lname'];
+		$mobile = $data['mobile'];
+		Database::disconnect();
+	}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Update a Disc Golf Player</h3>
+		    		</div>
+    		
+	    			<form class="form-horizontal" action="disc_player_update.php?id=<?php echo $id?>" method="post">
+					  <div class="control-group <?php echo !empty($fnameError)?'error':'';?>">
+					    <label class="control-label">First Name</label>
+					    <div class="controls">
+					      	<input name="fname" type="text"  placeholder="First Name" value="<?php echo !empty($fname)?$fname:'';?>">
+					      	<?php if (!empty($fnameError)): ?>
+					      		<span class="help-inline"><?php echo $fnameError;?></span>
+					      	<?php endif; ?>
+					    </div>
+					  </div>
+					  <div class="control-group <?php echo !empty($lnameError)?'error':'';?>">
+					    <label class="control-label">Last Name</label>
+					    <div class="controls">
+					      	<input name="lname" type="text" placeholder="Last Name" value="<?php echo !empty($lname)?$lname:'';?>">
+					      	<?php if (!empty($lnameError)): ?>
+					      		<span class="help-inline"><?php echo $lnameError;?></span>
+					      	<?php endif;?>
+					    </div>
+					  </div>
+					  
+					  <div class="control-group <?php echo !empty($mobileError)?'error':'';?>">
+					    <label class="control-label">Mobile Number</label>
+					    <div class="controls">
+					      	<input name="mobile" type="text"  placeholder="Description" value="<?php echo !empty($mobile)?$mobile:'';?>">
+					      	<?php if (!empty($mobileError)): ?>
+					      		<span class="help-inline"><?php echo $mobileError;?></span>
+					      	<?php endif;?>
+					    </div>
+					  </div>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-success">Update</button>
+						  <a class="btn" href="disc_player_list.php">Back</a>
+						</div>
+					</form>
+				</div>
+				
+    </div> <!-- /container -->
+  </body>
+</html>
