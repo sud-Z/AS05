0f3617d1c5e1b9d0d24782a7f39e1bd9285a4307
sud-Z <61097308+sud-Z@users.noreply.github.com>
2020-03-11 15:14:50 -0400

    Create disc_player_delete.php

---
+++ disc_player_delete.php
@@ -0,0 +1,67 @@
+<?php 
+	require 'database.php';
+	
+	session_start();
+    if(!isset($_SESSION["player_id"])){ // if "user" not set,
+	   session_destroy();
+	   header('Location: login.php');   // go to login page
+	   exit;
+    }
+    //$playerid = $_SESSION['player_id']; // for MyAssignments
+    $sessionid = $_SESSION['player_id'];
+    
+	$id = 0;
+	
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+	
+	if ( !empty($_POST)) {
+		// keep track post values
+		$id = $_POST['id'];
+		
+		// delete data
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "DELETE FROM players WHERE id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		
+		$sql = "DELETE FROM disc_events WHERE event_creator_id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		
+		Database::disconnect();
+		header("Location: login.php");
+	} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+   <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Delete an Event</h3>
+		    		</div>
+		    		
+	    			<form class="form-horizontal" action="disc_player_delete.php" method="post">
+	    			  <input type="hidden" name="id" value="<?php echo $id;?>"/>
+					  <p class="alert alert-error">Are you sure you want to delete your profile? this will delete all events you have created?</p>
+					  <div class="form-actions">
+						  <button type="submit" class="btn btn-danger">Yes</button>
+						  <a class="btn" href="disc_event_list.php">No</a>
+						</div>
+					</form>
+				</div>
+				
+    </div> <!-- /container -->
+  </body>
+</html>
